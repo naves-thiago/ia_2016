f9122a2a97631dc6c113c156ba244d662dce01b0
Thiago Naves <naves.thiago@gmail.com>
2016-09-30 22:59:26 -0300

    Adicionado A*, ainda com problemas

---
+++ a_star.py
@@ -0,0 +1,81 @@
+import heapq
+
+# Representa uma posicao para a qual podemos andar no mapa
+class No:
+    pos = (-1, -1)   # Posicao (x,y) no mapa
+    anterior = None  # De onde viemos
+    visitado = False # Ja visitamos esse no?
+    custo = 0
+    custo_acumulado = 0
+
+    # Construtor do objeto no. Sobrescreve as variaveis acima
+    def __init__(self, x, y, anterior=None, visitado=False, custo=0,
+                custo_acumulado=None):
+        self.pos = (x,y)
+        self.anterior = anterior
+        self.visitado = visitado
+        self.custo = custo
+        self.custo_acumulado = custo_acumulado
+
+    # Retorna uma string que representa esse no.
+    # nesse caso o par (x,y) da posicao
+    def __str__(self):
+        return str(self.pos)
+
+class A_star:
+    def _vizinhos(self, no):
+        ''' Retorna a lista de vizinhos do no '''
+        res = []
+        x, y = no.pos
+        if x > 0:
+            res.append(self.mapa[y][x-1])
+
+        if x < self.max_x:
+            res.append(self.mapa[y][x+1])
+
+        if y > 0:
+            res.append(self.mapa[y-1][x])
+
+        if y < self.max_y:
+            res.append(self.mapa[y+1][x])
+
+        return res
+
+    def _distancia(self, a, b):
+        ''' Calcula a distancia manhattan entre a e b.
+        a e b sao nos '''
+        return 0
+        #return abs(b.pos[0] - a.pos[0]) + abs(b.pos[1] - a.pos[1])
+
+    def __init__(self, mapa, pos_ini, pos_fim):
+        # Guarda o mapa
+        self.mapa = mapa
+        self.max_x = len(mapa[0])-1
+        self.max_y = len(mapa)-1
+        self.pos_fim = pos_fim
+
+        # Guarda os nos inicial e final
+        self.no_ini = mapa[pos_ini[1]][pos_ini[0]]
+        self.no_fim = mapa[pos_fim[1]][pos_fim[0]]
+
+        # Insere a posicao inicial
+        self.fronteira = [(0, self.no_ini)]
+        self.no_ini.custo_acumulado = 0
+
+    # Retorna True se terminou a busca
+    def step(self):
+        atual = heapq.heappop(self.fronteira)[1] # elem da heap = (custo, No)
+
+        if atual == self.no_fim:
+            return None
+
+        viz = self._vizinhos(atual)
+        for v in viz:
+            custo = atual.custo_acumulado + v.custo
+            if v.custo_acumulado == None or v.custo_acumulado > custo:
+                v.custo_acumulado = custo
+                prioridade = custo + self._distancia(self.no_fim, v)
+                heapq.heappush(self.fronteira, (prioridade, v))
+                v.anterior = atual
+
+        return (atual, viz)
---
+++ busca2.py
@@ -0,0 +1,94 @@
+import sys
+from a_star import *
+
+# Caracteres usados
+floresta = "D"
+inicio   = "I"
+fim      = "F"
+galho    = "G"
+lobo     = "C"
+vazio    = "."
+caminho  = "\x1b[41m \x1b[40m" # Colorido =)
+#caminho  = "_" # Sem cores
+
+# Custos dos caminhos
+custos = {floresta:200, galho:5, vazio:1, inicio:0, fim:0, lobo:0}
+
+max_x    = -1 # Posicao do ultimo char valido em cada linha do mapa
+max_y    = -1 # Numero de linhas -1 do mapa
+p_inicio = (-1,-1) # posicao de inicio (x,y)
+p_fim    = (-1,-1) # posicao de termino (x,y)
+
+mapa_s = None  # Mapa versao string, lido do arquivo
+mapa = []      # Mapa de No
+def carrega_mapa(arquivo):
+    global mapa, mapa_s, max_x, max_y, p_inicio, p_fim
+    mf = file(arquivo)
+    mapa_s = mf.readlines()
+    mf.close()
+    max_x = len(mapa_s[0])-3 # Remove \r\n
+    max_y = len(mapa_s)-1
+
+    # Cria nos para as posicoes livres do mapa
+    for x in range(max_x+1):
+        linha = []
+        mapa.append(linha)
+        for y in range(max_y+1):
+            linha.append(No(x, y, None, False, custos[mapa_s[y][x]]))
+
+            if mapa_s[y][x] == inicio:
+                p_inicio = (x,y)
+
+            if mapa_s[y][x] == fim:
+                p_fim = (x,y)
+
+# Retorna a lista de nos que representam o caminho percorrido
+def solucao(no):
+    res = []
+    atual = no
+    while atual != None:
+        res.append(atual)
+        atual = atual.anterior
+
+    return res
+
+# Imprime o mapa original
+def print_mapa():
+    for l in mapa_s:
+        sys.stdout.write(l)  # o mesmo que print, mas sem quebra de linha
+    print("")
+
+
+##### MAIN ######
+carrega_mapa("mapa_trabalho.txt")
+print("Inicio: "+str(p_inicio))
+print("Fim: "+str(p_fim))
+
+a = A_star(mapa, p_inicio, p_fim)
+
+while a.step() != None:
+    continue
+
+sol = solucao(a.no_fim)
+
+'''
+sol = bfs(p_inicio, p_fim)
+print_mapa()
+print("-------------------------------------\n")
+
+'''
+# Converte o mapa de string para vetor de char/strings
+mapa2 = []
+for l in mapa_s:
+    mapa2.append([x for x in l])
+
+# Altera o novo mapa para marcar o caminho
+for n in sol:
+    mapa2[n.pos[1]][n.pos[0]] = caminho
+
+# Converte de volta pra string para imprimir com a mesma cara
+for l in mapa2:
+    tmp = ""
+    for c in l:
+        tmp += c
+    sys.stdout.write(tmp)
