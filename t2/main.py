520e1bbf23ade7957d3e17c80870f2485713671f
Thiago Naves <naves.thiago@gmail.com>
2016-11-16 00:45:35 -0200

    Iniciada lógica para navegar para um No não visitado.

--- t2/main.py
+++ t2/main.py
@@ -1,12 +1,36 @@
 from map_loader import MapLoader
 from gui import Gui
 from prolog import PrologActor
+from a_star import A_star
 
 def update_map():
     pl.atualiza()
-    gui.set_map_actor(pl.mapa)
+    mapa_ator = pl.mapa
+    gui.set_map_actor(mapa_ator)
 
-# Program entry point
+def find_unvisited():
+    x, y = pl.pos
+    no_ini = mapa_ator[y][x]
+    a = A_star(mapa_ator, no_ini, pl.dir, None)
+    a.run()
+
+    # Encontra todos os nao visitados alcancaveis
+    candidatos = []
+    for y in mapa_ator:
+        for x in y:
+            if x.tipo == "?" and x.custo_acumulado != None:
+                candidatos.append(x)
+
+    # Econtra o No mais proximo entre os candidatos
+    melhor = candidatos[0]
+    for c in candidatos:
+        if c.custo_acumulado < melhor.custo_acumulado:
+            melhor = c
+
+    return melhor
+
+
+# Ponto de entrada do programa
 map_full = MapLoader("mapa.txt").mapa
 pl = PrologActor("teste.pl")
 gui = Gui(update_map)
