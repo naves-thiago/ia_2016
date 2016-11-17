fb497395247f94e5c0e92caa4a16f1bb25efbcda
jpaldi91 <jpaldi91@outlook.com>
2016-11-16 23:43:28 -0200

    pegar_powerup, pegar_ouro e consertou dano/morte

--- t2/novo.pl
+++ t2/novo.pl
@@ -231,16 +231,13 @@ observar :- posicao(P), assertThis(visitado(P)),
 sofrer :- posicao(P),
 (
 	/*se tem inimigo50, perde 50 pontos e toma 50 de dano*/
-	(m_inimigoD(P,_), pontos(Q), R is Q - 50, vida(V), U is V - 50, retractall(pontos(_)), assertThis(pontos(R)), retractall(vida(_)), assert(vida(U)), false);
+	(m_inimigoD(P,_), pontos(Q), R is Q - 50, vida(V), U is V - 50, retractall(pontos(_)), assertThis(pontos(R)), retractall(vida(_)), assert(vida(U)), assertThis(inimigo(P)), limpa_regiao_inimigo(P), vida(V), V < 1, pontos(Q), R is Q - 1000, retractall(pontos(_)), assertThis(pontos(R)), false);
 
 	/*se tem inimigo20, perde 20 pontos e toma 20 de dano*/
-	(m_inimigod(P,_), pontos(Q), R is Q - 20, vida(V), U is V - 20, retractall(pontos(_)), assertThis(pontos(R)), retractall(vida(_)), assert(vida(U)), false);
+	(m_inimigod(P,_), pontos(Q), R is Q - 20, vida(V), U is V - 20, retractall(pontos(_)), assertThis(pontos(R)), retractall(vida(_)), assert(vida(U)), assertThis(inimigo(P)), limpa_regiao_inimigo(P), vida(V), V < 1, pontos(Q), R is Q - 1000, retractall(pontos(_)), assertThis(pontos(R)), false);
 	
-	/*se tem buraco, perde e morre*/
-	(m_buraco(P), retractall(vida(_)), assert(vida(0)), false);
-	
-	/*se vida <= 0, perde 1000 pontos*/
-	(vida(V), V < 1, pontos(Q), R is Q - 1000, retractall(pontos(_)), assertThis(pontos(R)), false);
+	/*se tem buraco, morre e perde 1000 pontos*/
+	(m_buraco(P), retractall(vida(_)), assert(vida(0)), assertThis(buraco(P)), limpa_regiao_buraco(P), pontos(Q), R is Q - 1000, retractall(pontos(_)), assertThis(pontos(R)), false);
 	
 	/*se tem teleporte, eh teleportado, observa e sofre*/
 	(m_teleport(P), random_between(1, 12, X), random_between(1, 12, Y), retractall(posicao(_)), assert(posicao(p(X,Y))), observar);
@@ -272,6 +269,10 @@ atirar :- posicao(P), direcao(D), balas(M), M > 0, N is M - 1, retractall(balas(
 		m_inimigod(T, V), random_between(20, 50, Dano), NV is V - Dano, retractThis(m_inimigod(T,V)), assertThis(m_inimigod(T,NV)), NV < 1, mata_inimigod(T)
 	),
 	retractThis(tiro(T)).
+	
+pegar_ouro :- posicao(P), ouro(P), o_coletados(O), retract(ouro(P)), retract(m_ouro(P)), retract(o_coletados(O)), O1 is O + 1, assert(o_coletados(O1)), pontos(Q), R is Q + 1000, retract(pontos(Q)), assert(pontos(R)).
+
+pegar_powerup :- posicao(P), powerup(P), p_coletados(O), retract(powerup(P)), retract(m_powerup(P)), retract(p_coletados(O)), O1 is O + 1, assert(p_coletados(O1)), vida(Q), R is Q + 20, retract(vida(Q)), assert(vida(R)), R > 100, S is 100, retract(vida(R)), assert(vida(S)).
 
 /*SOMENTE PARA TESTE! APAGAR DEPOIS!!!*/
 andar_cima :- posicao(p(X,Y)), retractThis(posicao(p(X,Y))), NY is Y + 1, assertThis(posicao(p(X,NY))), observar.
